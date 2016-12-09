c921c6e4ac60d74405c2f6442e1ccee230d3c5bf
Victor Glimskog <victor.glimskog@gmail.com>
2016-12-09 13:28:04 +0100

    Added functionality for removing a player row and functionality so that the player numbers adjust automatically.

--- www/js/main.js
+++ www/js/main.js
@@ -1,12 +1,19 @@
 //wait for document ready
 $(function(){
 
-	//event listeners
+	//EVENT LISTENERS
 
-	//Event listener for the add new player button on start menu. 
-	//Adds new player line when clicked.
+	//event listener for the add new player button on start menu player form. 
+	//calls addNewPlayer from player-registration.js
 	$('.player-form').find('.row:last').on('click','button', function(){
 		var me = $(this);
 		addNewPlayer(me);
+	});
+
+	//event listener for the remove button on start menu player form.
+	//calls removePlayer from player-registration.js
+	$('.player-form').on('click', '.player button', function(){
+		var me = $(this);
+		removePlayer(me);
 	});
 });
--- www/js/player-registration.js
+++ www/js/player-registration.js
@@ -1,41 +1,74 @@
 // One or more globals
 var addNewPlayer;
+var removePlayer;
 
 // An anonymous self-executing function
 // all variables and functions declared within
 // are in a local scope
 (function(){
+	var playerCount;
 
-  function addNewPlayerFunc(thisButton){
-	var playerCount = $('.player').length / 1;
-	var nextPlayerNum = playerCount += 1;
+	function addNewPlayerFunc(thisButton){
+		var nextPlayerNum;
+
+		playerCount = $('.player').length / 1;
+		nextPlayerNum = playerCount += 1;
 
 		// add new player row
 		thisButton.closest('.row').before(
 			'<div class="row player">' +
-            '<div class="col-xs-3">' +
-                '<p class="form-label text-right"><strong>' + nextPlayerNum + '.</strong></p>' +
-              '</div>' +
-              '<div class="col-xs-6 center-block">' +
-                '<input type="text" class="form-control" id="playerNameInput' + nextPlayerNum + '"' +
-                'placeholder="Namn på spelare ' + nextPlayerNum + '">' +
-              '</div>' +
-              '<div class="col-xs-3">' +
-                '<button class="btn btn-danger"><span class="glyphicon glyphicon-remove"></span></button>' +
-              '</div>' +
-            '</div>' +
-            '<div class="vertical-spacer-m"></div>');
+			'<div class="col-xs-3">' +
+			'<p class="form-label text-right">' + nextPlayerNum + '.</p>' +
+			'</div>' +
+			'<div class="col-xs-6 center-block">' +
+			'<input type="text" class="form-control" id="playerNameInput' + nextPlayerNum + '"' +
+			'placeholder="Namn på spelare ' + nextPlayerNum + '">' +
+			'</div>' +
+			'<div class="col-xs-3">' +
+			'<button class="btn btn-danger"><span class="glyphicon glyphicon-remove"></span></button>' + '</div>' +
+			'</div>'
+		);
 
 		//focus the newly added player rows textfield
 		$('.player:last').find('input').focus();
 
-		//check to see if the "add" button should be disabled
+		//check to see if the "add" button should be disabled due to max player count
 		if(playerCount > 3){
 			thisButton.prop('disabled', true);
 		}
-  }
+	}
 
-  // Assign addNewPlayerFunc to global variable.
-  addNewPlayer = addNewPlayerFunc;
+	function removePlayerFunc(thisButton){
+		// remove the row whose button was clicked
+		thisButton.closest('.player').remove();
+		//decrease player count
+		playerCount = playerCount = $('.player').length / 1;
+
+		// check to see if addbutton should be enabled again and if so, enable it. 
+		if(playerCount < 4){
+			var addButton = $('.player-form').find('.row:last').find('button');
+			if(addButton.prop('disabled')){
+				addButton.prop('disabled', false);
+			}
+		}
+		updatePlayerForm();
+	}
+
+	// updates player numbers after deleting a row.
+	function updatePlayerForm(){
+		var thisPlayer;
+
+		$('.player').each(function( i ){
+			thisPlayer = $(this);
+
+			thisPlayer.find('.form-label').text( i+1+'.');
+			thisPlayer.find('input').attr('id', 'playerNameInput' + (i+1));
+			thisPlayer.find('input').attr('placeholder', 'Namn på spelare ' + (i+1));
+		});
+	}
+
+	// Assign addNewPlayerFunc to global variable.
+	addNewPlayer = addNewPlayerFunc;
+	removePlayer = removePlayerFunc;
 
 })();
