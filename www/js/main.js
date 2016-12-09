e2afa9d81780cdeea230e36b35d7a09f156eb5f2
Victor Glimskog <victor.glimskog@gmail.com>
2016-12-09 08:24:28 +0100

    Added a section in main.js for event listeners. Added an event listener for the add new player button on start menu.

--- www/js/main.js
+++ www/js/main.js
@@ -2,6 +2,11 @@
 $(function(){
 
 	//event listeners
-	
-	
+
+	//Event listener for the add new player button on start menu. 
+	//Adds new player line when clicked.
+	$('.player-form').find('.row:last').on('click','button', function(){
+		
+	});
+
 });
