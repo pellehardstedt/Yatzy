1f1a03ebb191fcba49b5b28934fe7e15ad71e021
Freddy Persson <FP_turtle@hotmail.com>
2016-12-11 13:10:11 +0100

    Adding the illusion of dices that "rolls"

--- www/js/diceRoll.js
+++ www/js/diceRoll.js
@@ -11,52 +11,50 @@ function startDiceRoll() {
 	$('.dice-area .roll-number').text(rollNumber);
 
 	$('.dice-area').on('click', 'button.roll', function() {
-
 		if(rollNumber > 0) {
-
 			rollAllDices();
-
 			rollNumber--;
-
 		}
-
 		$('.dice-area .roll-number').text(rollNumber);
-
 	});
 
 	//Locks the dice you click (adds class 'locked')
 	$('.dice-area').on('click', '.dices', function() {
-
 		if(rollNumber > 0 && rollNumber < 3) {
 			$(this).toggleClass('locked');
 		}
-
 	});
 
 	function rollAllDices() {
-
 		for(var i = 0; i < 5; i++) {
-
 			if($('.dice-area').find('#dice-' + (i + 1)).hasClass('locked')) {
-
 				continue;
-
 			}
-
+			rollDiceIllusion("dice-" + (i + 1));
 			var activeDiceRoll = oneDiceRoll();
-
 			theDiceRolls[i] = activeDiceRoll;
-
 			paintDiceRoll(activeDiceRoll, "dice-" + (i + 1));
-
 		}
 
 		function oneDiceRoll() {
-
 			var randomNumber = Math.floor((Math.random() * 6) + 1);
-
 			return randomNumber;
+		}
 
+		function rollDiceIllusion(dice) {
+			var activeTimesToRoll = timesToRoll();
+
+			while(activeTimesToRoll > 100) {
+				setTimeout(function(){
+					paintDiceRoll(oneDiceRoll(), dice);
+				}, activeTimesToRoll);
+				activeTimesToRoll -= 100;
+			}
+		}
+
+		function timesToRoll() {
+			var randomNumber = Math.floor((Math.random() * 700) + 700);
+			return randomNumber;
 		}
 	}
 }
