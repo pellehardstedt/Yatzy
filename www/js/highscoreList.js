b37f63fced40fd83da64c63a585511aff27b14cc
Pelle Härdstedt <pelle.hardstedt@gmail.com>
2016-12-12 12:17:32 +0100

    adding winner-function and some more

--- www/js/highscoreList.js
+++ www/js/highscoreList.js
@@ -1,26 +1,30 @@
 //declaring placeholder player objects
 var playerObject1 = {
+  playerNo: 1,
   name: "Per",
   score: 10,
 };
 
 var playerObject2 = {
+  playerNo: 2,
   name: "Maja",
   score: 12,
 };
 
 var playerObject3 = {
+  playerNo: 3,
   name: "Moroten",
   score: 5,
 };
 
 var playerObject4 = {
+  playerNo: 4,
   name: "Jägaren",
   score: 120,
 };
 
 //declaring the array to be sorted and placing player objects in it. should be a global variable.
-var result = [playerObject1, playerObject2, playerObject3, playerObject4];
+// var result = [playerObject1, playerObject2, playerObject3, playerObject4];
 
 //da real sorting being done
 function highscoreList(resultArray){
@@ -39,7 +43,7 @@ function highscoreList(resultArray){
 $('body').on('click', '#highscore-button', function(){
   //clear modal so that list dont duplicates when closing and opening.
   $('.modal-body-highscore tbody').empty();
-  highscoreList(result);
+  highscoreList(players);
   //for each element in 'result'
   result.forEach(function(player, i){
     $('.modal-tbody-highscore').append('<tr><td>' + (i+1) +'</td><td>'+ player.name + '</td><td>' + player.score + '</td></tr>');
