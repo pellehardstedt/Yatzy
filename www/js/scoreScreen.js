57220ec55cace349528d59e979b8498def2367dd
Björn Ohlsson <bjorn.ohlsson.93@gmail.com>
2016-12-19 14:30:32 +0100

    Edited scorescreen

--- www/js/scoreScreen.js
+++ www/js/scoreScreen.js
@@ -7,20 +7,19 @@ $(function() {
 function winnerScreen() {
 	//Shows name of winner of the game
 
+	$('.allWinners').prepend('<h4>4th: ' + "TestErik4"/*playersSorted[3].name*/ + ' Poäng: ' + "100"/*playersSorted[3].score*/ + '</h4>');
 
-	$('.allWinners').prepend('<h5>Plats 4: ' + "TestErik4"/*playersSorted[3].name*/ + ' Poäng: ' + "100"/*playersSorted[3].score*/ + '</h5>');
+	$('.allWinners').prepend('<h4>3rd: ' + "TestErik3"/*playersSorted[2].name*/ + ' Poäng: ' + "200"/*playersSorted[2].score*/ + '</h4>');
 
-	$('.allWinners').prepend('<h4>Plats 3: ' + "TestErik3"/*playersSorted[2].name*/ + ' Poäng: ' + "200"/*playersSorted[2].score*/ + '</h4>');
+	$('.allWinners').prepend('<h4>2nd: ' + "TestErik2"/*playersSorted[1].name*/ + ' Poäng: ' + "300"/*playersSorted[1].score*/ + '</h4>');
 
-	$('.allWinners').prepend('<h3>Plats 2: ' + "TestErik2"/*playersSorted[1].name*/ + ' Poäng: ' + "300"/*playersSorted[1].score*/ + '</h3>');
-
-	$('.allWinners').prepend('<h2>Plats 1: ' + "TestErik1"/*playersSorted[0].name*/ + ' Poäng: ' + "400"/*playersSorted[0].score*/ + '</h2>');
+	$('.allWinners').prepend('<h3>1st: ' + "TestErik1"/*playersSorted[0].name*/ + ' Poäng: ' + "400"/*playersSorted[0].score*/ + '</h3>');
 
 	$('.scoreScreen').show();
 	storeScore(playersSorted);
 }
 
-//When leaving scorescreen the screen clears
+//Pushing the restartbutton clears the screen
 function clearScoreScreen() {
     $('.allWinners').text("");
   }
