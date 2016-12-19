dece7fd328174372e06ffc264f4c46bbcf4fd824
Pelle Härdstedt <pelle.hardstedt@gmail.com>
2016-12-19 15:05:36 +0100

    More högpoäng

--- www/js/scoreScreen.js
+++ www/js/scoreScreen.js
@@ -10,11 +10,12 @@ function winnerScreen() {
 	playersRanked();
 
 	$('.allWinners').append('<h2>Plats 1: ' + playersSorted[0].name + ' Poäng: ' + playersSorted[0].scoreTotal + '</h2>');
-	if(players.length < 3) {
+	console.log(players.length);
+	if(players.length >= 2) {
 		$('.allWinners').append('<h3>Plats 2: ' + playersSorted[1].name + ' Poäng: ' + playersSorted[1].scoreTotal + '</h3>');
-		if(players.length < 4) {
+		if(players.length >= 3) {
 			$('.allWinners').append('<h4>Plats 3: ' + playersSorted[2].name + ' Poäng: ' + playersSorted[2].scoreTotal + '</h4>');
-			if(players.length < 5) {
+			if(players.length >= 4) {
 				$('.allWinners').append('<h5>Plats 4: ' + playersSorted[3].name + ' Poäng: ' + playersSorted[3].scoreTotal + '</h5>');
 			}
 		}
