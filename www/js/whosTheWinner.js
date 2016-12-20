66c7e28ac342ee4565ceab33561272c8d7f7262c
Pelle Härdstedt <pelle.hardstedt@gmail.com>
2016-12-20 11:55:59 +0100

    Sort and highscore

--- www/js/whosTheWinner.js
+++ www/js/whosTheWinner.js
@@ -6,10 +6,10 @@ function playersRanked(){
     //sort to 'playersSorted'
 
     playersSorted.sort(function (b, a) {
-        if (a.score > b.score) {
+        if (a.scoreTotal > b.scoreTotal) {
           return 1;
         }
-        if (a.score < b.score) {
+        if (a.scoreTotal < b.scoreTotal) {
           return -1;
         }
         return 0;
@@ -26,16 +26,16 @@ function storeScore(){
     playersSorted.forEach(function(playerActive, i){
       //check with each highscore-position
       if(parsedHighscore !== null){
-          parsedHighscore.forEach(function(playerHighscore, i){
-              if(playerActive.scoreTotal > playerHighscore.scoreTotal){
-                  //replace position with current player
-                  parsedHighscore.splice(i, 0, playerActive);
-                  if(parsedHighscore.length>=10){
-                    parsedHighscore.pop();
-                  }
-                  return;
-              }
-          });
+        for(var index = 0; parsedHighscore.length < index; index++){
+          if(playerActive.scoreTotal > parsedHighscore[index].scoreTotal){
+            //replace position with current player
+            parsedHighscore.splice(index, 0, playerActive);
+            if(parsedHighscore.length>10){
+              parsedHighscore.pop();
+            }
+            return;
+          }
+        }
         //Use parsedHighscore to update global array 'highscore'    
         highscore = parsedHighscore;
       }
@@ -43,79 +43,81 @@ function storeScore(){
         highscore.push(playerActive);
       }
     });
-    
+
     //stringify parsedHighscore to update the localStorage-highscore
     localStorage.setItem("highscore", JSON.stringify(highscore));
 }
 
 
-// var playerObject1 = {
-//   playerNo: 1,
-//   name: "Freddy",
-//   scoreBonus: 0,
-//   scoreTotal: 0,
-// };
+var playerObject1 = {
+  playerNo: 1,
+  name: "Freddy",
+  scoreBonus: 0,
+  scoreTotal: 120,
+};
 
-// var playerObject2 = {
-//   playerNo: 2,
-//   name: "Tommy",
-//   scoreBonus: 0,
-//   scoreTotal: 0,
-// };
+var playerObject2 = {
+  playerNo: 2,
+  name: "Tommy",
+  scoreBonus: 0,
+  scoreTotal: 110,
+};
 
-// var playerObject3 = {
-//   playerNo: 3,
-//   name: "Victor",
-//   scoreBonus: 0,
-//   scoreTotal: 0,
-// };
+var playerObject3 = {
+  playerNo: 3,
+  name: "Victor",
+  scoreBonus: 0,
+  scoreTotal: 100,
+};
 
-// var playerObject4 = {
-//   playerNo: 4,
-//   name: "Karl",
-//   scoreBonus: 0,
-//   scoreTotal: 0,
-// };
+var playerObject4 = {
+  playerNo: 4,
+  name: "Karl",
+  scoreBonus: 0,
+  scoreTotal: 70,
+};
 
-// var playerObject5 = {
-//   playerNo: 3,
-//   name: "Nanna",
-//   scoreBonus: 0,
-//   scoreTotal: 0,
-// };
-// var playerObject6 = {
-//   playerNo: 3,
-//   name: "Olle",
-//   scoreBonus: 0,
-//   scoreTotal: 0,
-// };
-// var playerObject7 = {
-//   playerNo: 3,
-//   name: "Fabian",
-//   scoreBonus: 0,
-//   scoreTotal: 0,
-// };
-// var playerObject8 = {
-//   playerNo: 3,
-//   name: "Beata",
-//   scoreBonus: 0,
-//   scoreTotal: 0,
-// };
-// var playerObject9 = {
-//   playerNo: 3,
-//   name: "Edde",
-//   scoreBonus: 0,
-//   scoreTotal: 0,
-// };
-// var playerObject10 = {
-//   playerNo: 3,
-//   name: "babian",
-//   scoreBonus: 0,
-//   scoreTotal: 0,
-// };
+var playerObject5 = {
+  playerNo: 3,
+  name: "Nanna",
+  scoreBonus: 0,
+  scoreTotal: 0,
+};
+var playerObject6 = {
+  playerNo: 3,
+  name: "Olle",
+  scoreBonus: 0,
+  scoreTotal: 0,
+};
+var playerObject7 = {
+  playerNo: 3,
+  name: "Fabian",
+  scoreBonus: 0,
+  scoreTotal: 0,
+};
+var playerObject8 = {
+  playerNo: 3,
+  name: "Beata",
+  scoreBonus: 0,
+  scoreTotal: 0,
+};
+var playerObject9 = {
+  playerNo: 3,
+  name: "Edde",
+  scoreBonus: 0,
+  scoreTotal: 0,
+};
+var playerObject10 = {
+  playerNo: 3,
+  name: "babian",
+  scoreBonus: 0,
+  scoreTotal: 0,
+};
 
-//declaring the array to be sorted and placing player objects in it. should be a global variable.
-var highscore = [];
+ //declaring the array to be sorted and placing player objects in it. should be a global variable.
+var highscore = [playerObject1, playerObject2, playerObject3, playerObject4, playerObject5];
+// var highscore = [];
+
 
 
 
