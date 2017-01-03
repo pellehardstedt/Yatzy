4651f606a0e285cc47b8cb19dcd1fece296dd57b
Victor Glimskog <victor.glimskog@gmail.com>
2017-01-03 11:06:43 +0100

    added a new function to bot-functions that finds the highest AVAILABLE (not filled in by player) result in array. It returns a td jquery object so that a bot can pass it to clickTd function in game.js. Otålige Ove is basically functioning but he ragequits when he rolls only zeros. Working on that right now.

--- www/js/bot-functions.js
+++ www/js/bot-functions.js
@@ -1,3 +1,4 @@
+//finds the highest value in an array and returns that value with its index contained in an object
 function findMax(resultsArray){
 	var max = 0;
 	var indexOfMax = 0;
@@ -15,4 +16,45 @@ function findMax(resultsArray){
 	returnObj.index = indexOfMax;
 
 	return returnObj;
+}
+
+//finds the highest value that is not allready filled in by the active player. Returns a td jquery object
+function findMaxOfAvailable(resultsArray){
+	var greenLight = false;
+	var bestResultObj;
+	var index,
+		result,
+		tableIndex;
+
+
+	while(greenLight === false){
+		//find the highest result in array
+		bestResultObj = findMax(resultsArray);
+
+		//dev
+		console.log(bestResultObj.index);
+		console.log(bestResultObj.result);
+
+		index = bestResultObj.index;
+		result = bestResultObj.result;
+
+		// assign tableIndex and if needed adjust the index taking the sum and total rows into account
+		if(index > 5){
+			tableIndex = (index + 2);
+		}
+		else{
+			tableIndex = index;
+		}
+
+		//find the active players td that corresponds to the best index in the results array
+		td = $('.score-table td.player-' + activePlayer.playerNo).eq(tableIndex);
+
+		//if the best result is allready filled in. Set it to 0 and find max again.
+		if( td.hasClass('filled-in-perm') ){
+			results[index] = 0;
+		}
+		else{
+			return td;
+		}
+	}
 }
--- www/js/otalige-ove.js
+++ www/js/otalige-ove.js
@@ -3,26 +3,13 @@ var runBotOO;
 
 (function(){
 	function runBotOOfunc(){
-		var index = 0;
-		var result = 0;
-		var td;
+		var tdToScore;
 
 		console.log(results);
 
-		var bestResultObj = findMax(results);
+		tdToScore = findMaxOfAvailable(results);
 
-		console.log(bestResultObj.index);
-		console.log(bestResultObj.result);
-
-		index = bestResultObj.index;
-		result = bestResultObj.result;
-
-		if(index > 5){
-			index += 2;
-		}
-
-		td = $('.score-table td.player-' + activePlayer.playerNo).eq(index);
-		clickTd(td);
+		clickTd(tdToScore);
 		console.log('done');
 		setTimeout(submitScore, 3000);
 	}
