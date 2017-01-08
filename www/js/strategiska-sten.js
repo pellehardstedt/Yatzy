35aaf8e7a1e7f8aa743b2db06887918d35923275
Freddy Persson <FP_turtle@hotmail.com>
2017-01-08 09:46:16 +0100

    Bot stuff (not done)

--- www/js/strategiska-sten.js
+++ www/js/strategiska-sten.js
@@ -1,4 +1,5 @@
 //otålige ove bot personality
+var whatToDoNextCalcFunc; //<---Remove later
 var runBotSS;
 
 (function(){
@@ -11,12 +12,11 @@ var runBotSS;
 	}
 
 	function makeCalculations(){
-		whatToDoNextCalc();
 
-		td = findMaxOfAvailable(results);
+		td = whatToDoNextCalc();
 
 		// If Otålige Ove has only 0s after a roll, he will roll one more time if he has available rolls. 
-		if(td === 0){
+		if(!td){
 			if(rollNumber > 0){
 				setTimeout(rollAgain, 2000);
 			}
@@ -26,21 +26,56 @@ var runBotSS;
 			}
 		}
 		else{
-			//call the submitFunction in bot-run.js , passing the chosed td to score.
+			//call the submitFunction in bot-run.js , passing the chosen td to score.
 			submitFunction(td);
 		}
 	}
 
 	function	whatToDoNextCalc(){
 
-		var botDecideObject = {};
+		var botDecideObject = {
+			doneTdList: []
+		},
+		dicesSorted = theDiceRolls.slice();
 
-		theDiceRolls.forEach(function(diceRoll, index) {
+    dicesSorted.sort(function (b, a) {
+      if (a > b) {
+        return 1;
+      }
+      if (a < b) {
+        return -1;
+      }
+      return 0;
+    });
 
-			for(var i = 1; i < (5 - index); i++) {
-				if(diceRoll === theDiceRolls[i]) {
-					//more then one of this number exists
-					botDecideObject.
+		//Saves all the tds that are filled-in-perm for this bot under a object property
+		$('.score-table td.player-'+activePlayer.playerNo)
+			.not('.no-preview')
+			.each(function(index) {
+				if($(this).hasClass('filled-in-perm')) {
+					botDecideObject.doneTdList.push(index);
+				}
+			});
+
+		dicesSorted.forEach(function(diceRoll, index) {
+
+			for(var i = index; i < 5; i++) {
+
+				//$.inArray(14, botDecideObject.doneTdList)
+
+				if(diceRoll === dicesSorted[i+1]) {
+					if(i+1 === 4 && $.inArray(14, botDecideObject.doneTdList) < 0) {
+						//return td position 14 (yatzy)
+					}	else if(i+1 === 3) {
+						//fyrtal av diceRoll
+						if(rollNumber === 0 && diceRoll <= 3 && $.inArray(diceRoll+1, botDecideObject.doneTdList) < 0) {
+							//retunera diceRoll+1 som td index (fyrtal av övre bonus delen)
+						} else if($.inArray(14, botDecideObject.doneTdList) < 0) {
+							//aim for yatzy
+						}
+					} else if(i+1 === 2) {
+						//only have 1 pair what we know
+					}
 				}
 			}
 		});
@@ -52,4 +87,5 @@ var runBotSS;
 	}
 
 	runBotSS = runBotSSfunc;
+	whatToDoNextCalcFunc = whatToDoNextCalc;
 })();
