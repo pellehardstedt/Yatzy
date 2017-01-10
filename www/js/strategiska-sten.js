3c8bf1ecf8beda7ebbd9c9c7f386e76dc9fbc6e9
Freddy Persson <FP_turtle@hotmail.com>
2017-01-10 15:13:18 +0100

    Bot sten, funka plz!

--- www/js/strategiska-sten.js
+++ www/js/strategiska-sten.js
@@ -31,25 +31,25 @@ var runBotSS;
 		}
 	}
 
-	function	whatToDoNextCalc(){
+	function whatToDoNextCalc(){
 
 		var botDecideObject = {
 			straight: []
-		}
+		};
 
 		var chances = Array.apply(null, Array(15)).map(function () {});
 
 		dicesSorted = theDiceRolls.slice();
 
-    dicesSorted.sort(function (b, a) {
-      if (a > b) {
-        return 1;
-      }
-      if (a < b) {
-        return -1;
-      }
-      return 0;
-    });
+		dicesSorted.sort(function (b, a) {
+			if (a > b) {
+				return 1;
+			}
+			if (a < b) {
+				return -1;
+			}
+			return 0;
+		});
 
 		//Saves all the tds that are filled-in-perm for this bot under a object property
 		$('.score-table td.player-'+activePlayer.playerNo)
@@ -69,11 +69,47 @@ var runBotSS;
 				botDecideObject.straight.push(diceRoll);
 			}
 
+			for(var i = 4; i > index; i--) {
+				if(diceRoll === dicesSorted[i]) {
+					
+					var numberOfSame = (i - index + 1);
+
+					chances[6] = 1;
+
+					if(diceRoll !== existingPair && existingPair !== 0) {
+						chances[7] = 1;
+						if(wasOrIsThreeOrMore === true) {
+							chances[12] = 1;
+						} else {
+							chances[12] = 2/6;
+						}
+					} else if(existingPair === 0) {
+						existingPair = diceRoll;
+						if(numberOfSame >= 3) {
+							wasOrIsThreeOrMore = true;
+							chances[8] = 1;
+						}
+					}
+
+					if(chances[14] !== 'done') {
+						chances[14] = Math.pow(1/6, 5 - numberOfSame);
+					}
+					if(chances[diceRoll-1] === undefined && chances[diceRoll-1] !== 'done') {
+						chances[diceRoll-1] = Math.pow(1/6, 5 - numberOfSame);
+					}
+					if(chances[6] !== 'done') {
+						chances[6] = 1;
+					}
+				}
+			}
+
+			if(chances[diceRoll-1] === undefined && chances[diceRoll-1] !== 'done') {
+				chances[diceRoll-1] = Math.pow(1/6, 4);
+			}
+
 			if(index === 4) {
 
 				var straightLength = botDecideObject.straight.length;
-
-				console.log(straightLength);
 
 				if(chances[10] !== 'done') {
 					if($.inArray(1, dicesSorted) >= 0) {
@@ -90,61 +126,117 @@ var runBotSS;
 						chances[11] = Math.pow(1/6, 5 - straightLength);
 					}
 				}
-			}
 
-			for(var i = 4; i > index; i--) {
+				if(dicesSorted[4] === 1 &&
+					chances[0] === undefined &&
+					chances[0] !== 'done') {
+					chances[0] = Math.pow(1/6, 4);
+				}
 
-				if(diceRoll === dicesSorted[i]) {
+				for(var i = 0; i < 6; i++) {
+					if(chances[i] !== 'done' && chances[i] === undefined) {
+						chances[i] = Math.pow(1/6, 5);
+					}
+				}
 
-					var numberOfSame = (i - index + 1);
-
-					chances[6] = 1;
-
-					if(diceRoll !== existingPair && existingPair !== 0) {
-						chances[7] = 1;
-						if(wasOrIsThreeOrMore === true) {
-							chances[12] = 1;
-						}
-					} else if(existingPair === 0) {
-						existingPair = diceRoll;
-						if(numberOfSame >= 3) {
-							wasOrIsThreeOrMore = true;
-							chances[6] = 1;
-						}
-					}
-
-					if(numberOfSame === 5 && chances[14] !== 'done') {
-						chances[14] = 1;
-					}
-
-					if(chances[diceRoll-1] !== 'done') {
-						chances[diceRoll-1] = Math.pow(1/6, 5 - numberOfSame); //Not sure if this should be changed if you have x score in the bucket
-					}
-
-					if(chances[6] !== 'done') {
-						chances[6] = 1;
-					}
-				} else {
-					if(chances[6] == false) {
-						chances[6] = 4/6;
-					}
-					if(chances[7] == false) {
-						chances[7] = 4/6*3/6*2/6; //Chance for two pairs
-					}
-					if(chances[8] == false) {
-						chances[8] = 4/6*3/6; //Chance for threeOfAKind
-					}
-					if(chances[9] == false) {
-						chances[9] = 4/6*3/6*2/6*1/6; //Chance for fourOfAKind
-					}
+				if(chances[6] === undefined) {
+					chances[6] = 7056/7776;
+				}
+				if(chances[7] === undefined) {
+					chances[7] = 4/6*3/6*2/6; //Chance for two pairs
+				}
+				if(chances[8] === undefined) {
+					chances[8] = 1656/7776; //Chance for threeOfAKind
+				}
+				if(chances[9] === undefined) {
+					chances[9] = 4/6*3/6*2/6*1/6; //Chance for fourOfAKind
+				}
+				if(chances[14] === undefined) {
+					chances[14] = 4/6*3/6*2/6*1/6; //Chance for yatzy
+				}
+				if(chances[12] === undefined) {
+					chances[12] = 4/6*3/6*2/6*1/6; //Chance for yatzy
 				}
 			}
 		});
 		console.log(chances);
+
+		var biggestChanceIndexBonus1 = 0;
+		var biggestChanceIndexBonus2 = 0;
+		var biggestChanceIndexNoBonus = 0;
+
+		for(var i = 1; i <= chances.length; i++) {
+
+			if(chances[i] > chances[biggestChanceIndexBonus1] && i < 6) {
+
+				biggestChanceIndexBonus1 = i;
+
+			} else if(chances[i] = chances[biggestChanceIndexBonus1] && i < 6) {
+
+				biggestChanceIndexBonus2 = i;
+
+			}
+			
+			if(chances[i] > chances[biggestChanceIndexNoBonus] && i > 5) {
+
+				biggestChanceIndexNoBonus = i;
+			}
+		}
+		
+		if(chances[biggestChanceIndexBonus1] >= chances[biggestChanceIndexNoBonus]) {
+			if(chances[biggestChanceIndexBonus1] > 0.0008 && 
+				chances[biggestChanceIndexBonus2] < 0.0008) {
+				// pair of biggestChanceIndexBonus1 exists
+				if(activePlayer.bonus === false) {
+					lockAllOfSame(biggestChanceIndexBonus1+1);
+				}
+			} else if(chances[biggestChanceIndexBonus1] > 0.0008 && chances[biggestChanceIndexBonus2] > 0.0008) {
+				// two pairs exists
+				lockAllOfSame(biggestChanceIndexBonus1+1);
+				lockAllOfSame(biggestChanceIndexBonus2+1);
+			}
+			if(rollNumber > 0) {
+				rollAgain();
+			} else if(biggestChanceIndexBonus1 > biggestChanceIndexBonus2) {
+				return biggestChanceIndexBonus1;
+			} else {
+				return biggestChanceIndexBonus2;
+			}
+		} else {
+			if(biggestChanceIndexNoBonus === 10 || biggestChanceIndexNoBonus === 11) {
+				lockAllStraight(biggestChanceIndexNoBonus);
+			}
+			if(rollNumber > 0) {
+				rollAgain();
+			} else {
+				return biggestChanceIndexNoBonus; 
+			}
+		}
+	}
+
+	function lockAllOfSame(dicesToLock){
+		for(var i2 = 0; i2 < 5; i2++) {
+			if(theDiceRolls[i2] === dicesToLock) {
+				lockDices($('.dice-area .canvas-lock-area').eq(i2));
+			}
+		}
+	}
+
+	function lockAllStraight(largeOrSmall){
+		for(var i2 = 0; i2 < botDecideObject.straight.length; i2++) {
+			var indexOfStraight = $.inArray(botDecideObject.straight[i2], theDiceRolls);
+			lockDices($('.dice-area .canvas-lock-area').eq(indexOfStraight));
+		}
+		if(largeOrSmall === 11) {
+			lockDices($('.dice-area .canvas-lock-area').eq($.inArray(6, theDiceRolls)));
+		} else {
+			lockDices($('.dice-area .canvas-lock-area').eq($.inArray(1, theDiceRolls)));
+		}
 	}
 
 	function rollAgain(){
 		rollDices();
+		clearDices();
 		makeCalculations();
 	}
 
